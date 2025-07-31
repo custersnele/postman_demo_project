060085f492ed006b0c989b8e313f9557d263897f
Nele Custers <custersnele@gmail.com>
2025-07-31 16:30:54 +0200

    Create sign.js

---
+++ .github/workflows/api-tests/scripts/sign.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+ 
+const privateKey = process.env.JSB_PRIVATE_KEY;
+ 
+const base64Payload = process.argv[2];
+const jsonString = Buffer.from(base64Payload, 'base64').toString('utf8');
+ 
+// Parse to object
+const payload = JSON.parse(jsonString);
+ 
+const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+console.log(token);
